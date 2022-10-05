fa3db91600d3f4586d9bc65401c0d01905ffe8cb
Julian Cardone <juliancardone1@gmail.com>
2022-10-05 14:29:53 -0400

    worked on pawns

--- chess/pieces/pawn.rb
+++ chess/pieces/pawn.rb
@@ -1,53 +1,60 @@
 class Pawn < Piece
+
+        FORWARD_STEPS = [
+            [1,0],
+            [2,0]
+        ].freeze
+
+        SIDE_ATTACKS = [
+            [1, -1], 
+            [1, 1]
+        ].freeze
+
     def moves
 
     end
 
     private
     def at_start_row?
-        row, col = pos
-        if color == "WHITE"
-            if row == 1
-                return true
-            else
-                return false
-            end
+        row = self.pos[0]
+        color = self.color
+        if color == :white
+            row == 6 ? return true : return false
         else
-            if row == 6
-                return true
-            else
-                return false
-            end
+            row == 1 ? return true : return false 
         end
     end
 
     def forward_dir
-        if self.at_start_row?
-            return 1
-        else
-            return -1
-        end
+        self.color == :white ? -1 : 1
     end
 
     def forward_steps
+
+        dir = self.forward_dir
+
+        steps = FORWARD_STEPS.map {|step| dir * step}
+
         if self.at_start_row?
-            if self.color == "WHITE"
-                
-
-            end
+            return steps
         else
-
+            return [steps[0]]
         end
 
-        return [[1, -1], [1, 0], [2, 0], [1, 1]]
     end
 
     def side_attacks
-        if self.color == "WHITE"
-            diagonals = [[1, -1], [1, 1]]
-            diagonals.each do |diagonal|
-                dx, dy = diagonal
-                new_pos = [pos[0] + diagonal[0], pos[1] + diagonal[1]]
+       
+        dir = self.forward_dir
+
+        attacks = SIDE_ATTACKS.map {|step| dir * step}
+
+        attacks.each do |attack|
+            new_pos = [pos[0] + attack[0], pos[1] + attack[1]]
+            if !self[new_pos].empty? && self[new_pos].color != color 
+                result << new_pos
             end
+        end
+        return result
     end
 end
